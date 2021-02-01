2528030106718bb0adb7c0ab7db9cebccca342d5
Robert Gallacher <rjgallac@hotmail.com>
2021-02-01 19:20:30 +0000

    clean up

--- index.js
+++ index.js
@@ -2,20 +2,8 @@ const googleAuth = require('./google-auth');
 const {getBookable, getBooked} = require("./google-calendar-function");
 const {splitSlots, removeConflicts} = require("./slot-util");
 
-var authen;
 googleAuth.login()
-.then(auth =>{
-  authen = auth
-  return getBookable(auth);
-})
-.then(bookableSlots => {
-  slots = splitSlots(bookableSlots);
-  getBooked(authen)
-  .then(bookedData =>{
-    console.log(slots)
-
-    console.log(bookedData);
-    const results = removeConflicts(slots, bookedData)
-    console.log(results)
-  })
-});  
+.then(auth => Promise.all([getBookable(auth), getBooked(auth)])
+.then(data => {
+  console.log(removeConflicts(splitSlots(data[0]), data[1]));
+}));  
